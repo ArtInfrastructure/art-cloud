5c25892b6dc423831825e79878acacc5ded0746b
Trevor F. Smith <trevor@trevor.smith.name>
2009-01-23 09:54:29 -0800

    Fixed the url patterns.

--- art_cloud/front/urls.py
+++ art_cloud/front/urls.py
@@ -3,7 +3,5 @@ from django.conf import settings
 
 urlpatterns = patterns('',
     (r'^$', 'art_cloud.front.views.index'),
+	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
-
-if settings.DEBUG:
-	urlpatterns.append((r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}))
