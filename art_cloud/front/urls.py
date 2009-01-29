6a1b65e7d335d90cf965518197eec860155a522b
Trevor F. Smith <trevor@trevor.smith.name>
2009-01-29 13:34:33 -0800

    Set up login and logout features.

--- art_cloud/front/urls.py
+++ art_cloud/front/urls.py
@@ -8,7 +8,12 @@ urlpatterns = patterns('',
     (r'^installation-site/(?P<id>[\d]+)/$', 'art_cloud.front.views.installation_site_detail'),
     (r'^installation/(?P<id>[\d]+)/$', 'art_cloud.front.views.installation_detail'),
     (r'^profile/(?P<username>[^/]+)/$', 'art_cloud.front.views.profile_detail'),
+	(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'front/login.html'}),
+	(r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
+	(r'^accounts/profile/$', 'django.views.generic.simple.redirect_to', {'url': '/'}),
+
     (r'^$', 'art_cloud.front.views.index'),
 
 	(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+	
 )
--- art_cloud/front/views.py
+++ art_cloud/front/views.py
@@ -18,6 +18,7 @@ import django.contrib.contenttypes.models as content_type_models
 from django.template import RequestContext
 from django.core.cache import cache
 from django.core.mail import send_mail
+from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.template.loader import render_to_string
 from django.utils import feedgenerator
@@ -25,17 +26,17 @@ from django.utils import feedgenerator
 from models import *
 from forms import *
 
-@staff_member_required
+@login_required
 def index(request):
-	return render_to_response('front/index.html', { 'profiles':UserProfile.objects.all(), 
+	return render_to_response('front/index.html', { 'profiles':UserProfile.objects.filter(user__groups__name="artists"), 
 													'sites':InstallationSite.objects.all(), 
 													'installations':Installation.objects.all(),
 													}, context_instance=RequestContext(request))
 
-@staff_member_required
+@login_required
 def profile_detail(request, username):
 	profile = get_object_or_404(UserProfile, user__username=username)
-	if request.method == 'POST':
+	if request.method == 'POST' and request.user.is_staff:
 		user_form = UserForm(request.POST, instance=profile.user)
 		user_profile_form = UserProfileForm(request.POST, instance=profile)
 		if user_form.is_valid():
@@ -46,27 +47,30 @@ def profile_detail(request, username):
 	profile = get_object_or_404(UserProfile, user__username=username)
 	user_form = UserForm(instance=profile.user)
 	user_profile_form = UserProfileForm(instance=profile)
-	return render_to_response('front/profile_detail.html', { 'profile':profile, 'user_form':user_form, 'user_profile_form':user_profile_form }, context_instance=RequestContext(request))
+	return render_to_response('front/profile_detail.html', { 'profile':profile, 'user_form':user_form, 
+															'user_profile_form':user_profile_form }, context_instance=RequestContext(request))
 
-@staff_member_required
+@login_required
 def photo_detail(request, id):
-	return render_to_response('front/photo_detail.html', {}, context_instance=RequestContext(request))
+	photo = get_object_or_404(Photo, pk=id)
+	return render_to_response('front/photo_detail.html', { 'photo':photo }, context_instance=RequestContext(request))
 
-@staff_member_required
+@login_required
 def equipment_type_detail(request, id):
 	return render_to_response('front/equipment_type_detail.html', {}, context_instance=RequestContext(request))
 
-@staff_member_required
+@login_required
 def equipment_detail(request, id):
 	return render_to_response('front/equipment_detail.html', {}, context_instance=RequestContext(request))
 
-@staff_member_required
+@login_required
 def installation_site_detail(request, id):
 	site = get_object_or_404(InstallationSite, pk=id)
 	print dir(site)
 	return render_to_response('front/installation_site_detail.html', { 'site':site }, context_instance=RequestContext(request))
 
-@staff_member_required
+@login_required
 def installation_detail(request, id):
 	installation = get_object_or_404(Installation, pk=id)
 	return render_to_response('front/installation_detail.html', { 'installation':installation }, context_instance=RequestContext(request))
+
